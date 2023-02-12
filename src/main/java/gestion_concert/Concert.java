914e336335bb462b315ddbf3a0fbf734346b9387
youssef <youssef.kadri@etudiant.univ-brest.fr>
2023-02-12 18:00:36 +0100

    [FIX] conflus de la version JAVA

--- src/main/java/gestion_concert/Concert.java
+++ src/main/java/gestion_concert/Concert.java
@@ -5,6 +5,10 @@
 
 package gestion_concert;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
 /**
  *
  * @author amrii
@@ -12,6 +16,18 @@ package gestion_concert;
 public class Concert {
 
     public static void main(String[] args) {
-        System.out.println("Hello Worldgrfnfg!");
-    }
+        System.out.println("Helloggggg Worldgrfnfg!");
+     // charge le gestionnaire d'entités lié à l'unité de persistance "SportsPU"
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("ConcertPU");
+        EntityManager em = emf.createEntityManager();
+        System.out.println("PU chargée");
+        // récupère le sport d'identifiant 1, affiche son intitulé et ses disciplines
+        int cle = 1;
+        TSalleSal salle = em.find(TSalleSal.class, cle);
+        // System.out.println("Le sport " + cle + " est " + salle.getIntitule());
+        for (TSoireeSoi disc : salle.getTSoireeSoiCollection())
+        System.out.println(" -> " + disc.getSalId());
+        }
+
+    
 }
